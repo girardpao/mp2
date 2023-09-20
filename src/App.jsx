38605ec74edbb76b33fb8a2709725bf28ab6fd02
geneawesome <genecolumna@gmail.com>
2023-09-20 08:30:32 +0800

    Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,13 @@
+import React from 'react'
+import TopNav from './components/Topnav'
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <TopNav/>
+    </div>
+  );
 };
 
 export default App;
---
+++ src/components/TopNav.jsx
@@ -0,0 +1,53 @@
+import React from 'react'
+import { Container, Navbar, Nav, NavLink} from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+const TopNav = () => {
+
+    const menuData = [
+    {
+        path:'/',
+        name: "Home"
+    },
+    {
+        path:'/about',
+        name: "About"
+    },
+    {
+        path:'/contact',
+        name: "Contact"
+    },
+    {
+        path:'/signup',
+        name: "SignUp"
+    },
+    {
+        path:'/whyjoin',
+        name: "WhyJoin"
+    },
+    ]
+  return (
+    <Navbar bg="light" expand="lg">
+      <Container>
+        <Navbar.Brand href="#home" className="brand">Gymfinity</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ms-auto">
+            {
+                menuData.map((item)=>(
+                    <NavLink to={item.path} key={item.name}>
+                        <div className="list_item">{item.name}</div>
+                        </NavLink>
+                ))
+            }
+
+          </Nav>
+          <Nav className="ms-auto">
+            <button className="btn btn-success">Sign Up</button>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  )
+}
+
+export default TopNav;
