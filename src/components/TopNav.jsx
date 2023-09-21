e96fad0263dc63e9db91c2d45a2e768de390f47e
girardpao <girardpao@gmail.com>
2023-09-21 12:55:30 +0800

    test

--- src/components/TopNav.jsx
+++ src/components/TopNav.jsx
@@ -1,42 +1,38 @@
-import { Container, Navbar, Nav, NavLink} from 'react-bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
-
+import { Container, Navbar, Nav, NavLink } from "react-bootstrap";
 
 const TopNav = () => {
-
-    const menuData = [
+  const menuData = [
     {
-        path:'/',
-        name: "Home"
+      path: "/",
+      name: "Home",
     },
     {
-        path:'/about',
-        name: "About"
+      path: "/about",
+      name: "About",
     },
     {
-        path:'/contact',
-        name: "Contact"
+      path: "/contact",
+      name: "Contact",
     },
     {
-        path:'/whyjoin',
-        name: "WhyJoin"
+      path: "/whyjoin",
+      name: "WhyJoin",
     },
-    ]
+  ];
   return (
     <Navbar bg="light" expand="lg">
       <Container>
-        <Navbar.Brand href="#home" className="brand">Gymfinity</Navbar.Brand>
+        <Navbar.Brand href="#home" className="brand">
+          Gymfinity
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ms-auto">
-            {
-                menuData.map((item)=>(
-                    <NavLink to={item.path} key={item.name}>
-                        <div className="list_item">{item.name}</div>
-                        </NavLink>
-                ))
-            }
-
+            {menuData.map((item) => (
+              <NavLink to={item.path} key={item.name}>
+                <div className="list_item">{item.name}</div>
+              </NavLink>
+            ))}
           </Nav>
           <Nav className="ms-auto">
             <button className="btn btn-success">Sign Up</button>
@@ -44,7 +40,7 @@ const TopNav = () => {
         </Navbar.Collapse>
       </Container>
     </Navbar>
-  )
-}
+  );
+};
 
 export default TopNav;
