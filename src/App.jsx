01af693f65f7cf8855f771e6507c7aa29763dbc9
geneawesome <genecolumna@gmail.com>
2023-09-22 09:59:00 +0800

    update the links

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,26 @@ import TopNav from "./components/Topnav";
 import { Routes, Route } from "react-router-dom";
 import routes from "./routes";
 import "./App.css";
-
+import About from "./views/Ui/About/About";
+import Contact from "./views/Ui/Contact/Contact";
+import SignUp from "./views/Ui/SignUp/SignUp";
+import WhyJoin from "./views/Ui/WhyJoin/WhyJoin";
 
 const App = () => {
+  console.log(routes)
+  
   return (
     <div className="App">
       <TopNav />
+      <Routes>
+        <Route path = "/about" element = {<About/>}/>
+        <Route path = "/contact" element = {<Contact/>}/>
+        <Route path = "/signup" element = {<SignUp/>}/>
+        <Route path = "/whyjoin" element = {<WhyJoin/>}/>
+
+
+        
+      </Routes>
     </div>
   );
 };
--- src/routes.jsx
+++ src/routes.jsx
@@ -3,8 +3,9 @@ import Contact from "./views/Ui/Contact/Contact";
 import SignUp from "./views/Ui/SignUp/SignUp";
 import WhyJoin from "./views/Ui/WhyJoin/WhyJoin";
 
+
 const routes = [
-    { path: "/", element: <About />,},
+    { path: "/about", element: <About />,},
     { path: "/contact", element: <Contact />,},
     { path: "/signUp", element: <SignUp />,},
     { path: "/whyJoin", element: <WhyJoin />,},  
