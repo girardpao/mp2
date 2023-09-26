19df4eff6dde35c02469e5903edb78309649804f
geneawesome <genecolumna@gmail.com>
2023-09-27 07:02:37 +0800

    Navbarupdata and Footerupdate

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => {
     <footer class="footer-distributed">
 
         <div class="footer-left">
-            <h3>Contact<span> Me</span></h3>
+            <h3>Contact<span> Us</span></h3>
 
             <p class="footer-links">
                 <a href="#">Email</a>
@@ -21,8 +21,8 @@ const Footer = () => {
         <div class="footer-center">
             <div>
                 <i class="fa fa-map-marker"></i>
-                <p><span>Tuguegarao City</span>
-                   Cagayan </p>
+                <p><span>790 P. Ocampo Sr. St.</span>
+                formerly, Ocampo St, Malate, Manila, 1004 Metro Manila)</p>
             </div>
 
             <div>
@@ -31,7 +31,7 @@ const Footer = () => {
             </div>
             <div>
                 <i class="fa fa-envelope"></i>
-                <p><a href="#">awesomeakatsuki@gmail.com</a></p>
+                <p><a href="#">Gyminfinity@gmail.com</a></p>
             </div>
         </div>
         <div class="footer-right">
