a63c5dc32bb7c963d597e0ae4850fad8236eafec
girardpao <girardpao@gmail.com>
2023-09-28 13:54:36 +0800

    on footer, changed class to className

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,94 +1,109 @@
-import React from 'react'
-import "./Footer.css"
+import React from "react";
+import "./Footer.css";
 const Footer = () => {
-
   return (
     <footer className="footer">
-	<div className="container">
-		<div className="row">
-			<div className="footer-col">
-				<h4>Gymfinity</h4>
-				<ul>
-					<li><a href="#">About Us</a></li>
-					<li><a href="#">Our Services</a></li>
-					<li><a href="#">Programs</a></li>   
+      <div className="container">
+        <div className="row">
+          <div className="footer-col">
+            <h4>Gymfinity</h4>
+            <ul>
+              <li>
+                <a href="#">About Us</a>
+              </li>
+              <li>
+                <a href="#">Our Services</a>
+              </li>
+              <li>
+                <a href="#">Programs</a>
+              </li>
+            </ul>
+          </div>
 
-				</ul>
-			</div>
+          <div className="footer-col">
+            <h4>Get Help</h4>
+            <ul>
+              <li>
+                <a href="#">FAQ</a>
+              </li>
+              <li>
+                <a href="#">Shipping</a>
+              </li>
+              <li>
+                <a href="#">Returns</a>
+              </li>
+              <li>
+                <a href="#">Order Status</a>
+              </li>
+              <li>
+                <a href="#">Payment Options</a>
+              </li>
+            </ul>
+          </div>
+          <div className="footer-col">
+            <h4>Follow US</h4>
+            <div className="social-links">
+              <a href="#">
+                <i className="fab fa-facebook-f"></i>
+              </a>
+              <a href="#">
+                <i className="fab fa-twitter"></i>
+              </a>
+              <a href="#">
+                <i className="fab fa-instagram"></i>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
 
-			<div className="footer-col">
-				<h4>Get Help</h4>
-				<ul>
-					<li><a href="#">FAQ</a></li>
-					<li><a href="#">Shipping</a></li>
-					<li><a href="#">Returns</a></li>
-					<li><a href="#">Order Status</a></li>
-					<li><a href="#">Payment Options</a></li>
+    //     <footer class="footer-distributed">
 
+    //         <div class="footer-left">
+    //             <h3>Contact<span> Us</span></h3>
 
-				</ul>
-			</div>
-			<div className="footer-col">
-				<h4>Follow US</h4>
-				<div className="social-links">
-					<a href="#"><i class="fab fa-facebook-f"></i></a>
-					<a href="#"><i class="fab fa-twitter"></i></a>
-					<a href="#"><i class="fab fa-instagram"></i></a>
+    //             <p class="footer-links">
+    //                 <a href="#">Email</a>
+    //                 <a href="#">Account</a>
+    //                 <a href="#">Subcribe</a>
+    //                 <a href="#">Blog</a>
+    //             </p>
 
+    //             <p class="footer-company-name">Copyright © 2023 All rights reserved</p>
+    //         </div>
 
-				</div>
-			</div>
-		</div>
-	</div>
+    //         <div class="footer-center">
+    //             <div>
+    //                 <i class="fa fa-map-marker"></i>
+    //                 <p><span>790 P. Ocampo Sr. St.</span>
+    //                 (formerly, Ocampo St, Malate, Manila, 1004 Metro Manila)</p>
+    //             </div>
 
-</footer>
- 
-//     <footer class="footer-distributed">
+    //             <div>
+    //                 <i class="fa fa-phone"></i>
+    //                 <p>096763179108</p>
+    //             </div>
+    //             <div>
+    //                 <i class="fa fa-envelope"></i>
+    //                 <p><a href="#">Gyminfinity@gmail.com</a></p>
+    //             </div>
+    //         </div>
+    //         <div class="footer-right">
+    //             <p class="footer-company-about">
+    //                 <span>Social Media</span>
+    //                 <strong>Contact me at my Social Media Accounts</strong>
 
-//         <div class="footer-left">
-//             <h3>Contact<span> Us</span></h3>
+    //             </p>
+    //             <div class="footer-icons">
+    //                 <a href="#"><i class="fa fa-facebook"></i></a>
+    //                 <a href="#"><i class="fa fa-instagram"></i></a>
+    //                 <a href="#"><i class="fa fa-twitter"></i></a>
+    //             </div>
+    //         </div>
 
-//             <p class="footer-links">
-//                 <a href="#">Email</a>
-//                 <a href="#">Account</a>
-//                 <a href="#">Subcribe</a>
-//                 <a href="#">Blog</a>
-//             </p>
-
-//             <p class="footer-company-name">Copyright © 2023 All rights reserved</p>
-//         </div>
-
-//         <div class="footer-center">
-//             <div>
-//                 <i class="fa fa-map-marker"></i>
-//                 <p><span>790 P. Ocampo Sr. St.</span>
-//                 (formerly, Ocampo St, Malate, Manila, 1004 Metro Manila)</p>
-//             </div>
-
-//             <div>
-//                 <i class="fa fa-phone"></i>
-//                 <p>096763179108</p>
-//             </div>
-//             <div>
-//                 <i class="fa fa-envelope"></i>
-//                 <p><a href="#">Gyminfinity@gmail.com</a></p>
-//             </div>
-//         </div>
-//         <div class="footer-right">
-//             <p class="footer-company-about">
-//                 <span>Social Media</span>
-//                 <strong>Contact me at my Social Media Accounts</strong>
-                
-//             </p>
-//             <div class="footer-icons">
-//                 <a href="#"><i class="fa fa-facebook"></i></a>
-//                 <a href="#"><i class="fa fa-instagram"></i></a>
-//                 <a href="#"><i class="fa fa-twitter"></i></a>
-//             </div>
-//         </div>
-    
-// </footer> 
-  )
-}
+    // </footer>
+  );
+};
 
 export default Footer;
